478b17a032911016ffa0e861e3ad1ed1da5d146f
Anthony M. Bonafide <AnthonyMBonafide@gmail.com>
2024-02-23 16:43:37 -0500

    Add Functionality to Insert Items
    Update Hashmap to not use static arrays which would not allow the
    Hashmap to grow beyond its defined size, but rather use Vec so that the
    size can be controlled for better performance.
    
    Signed-off-by: Anthony M. Bonafide <AnthonyMBonafide@gmail.com>

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -1,31 +1,85 @@
-use std::u64;
+use std::{
+    hash::{DefaultHasher, Hash, Hasher},
+    rc::Rc,
+    u64,
+};
 
 const DEFAULT_SIZE: u64 = 256;
+#[derive(Debug)]
 pub struct MyHashmap<K, V> {
     // A guess at how many elements the hashmap will manage.
     size: u64,
 
     // The "table" which will hold all the data
-    hash_elements: Vec<Option<KeyValue<K, V>>>,
+    hash_elements: Vec<Bucket<K, V>>,
     hasher: fn(key: K) -> usize,
 }
+#[derive(Debug, Clone)]
+struct Bucket<K, V> {
+    head: Option<Rc<KeyValue<K, V>>>,
+    tail: Option<Rc<KeyValue<K, V>>>,
+}
+
+impl<K, V> Bucket<K, V>
+where
+    K: PartialEq,
+{
+    pub fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn find(&self, key: K) -> Option<&V> {
+        let mut c = &self.head;
+        while let Some(kv) = c {
+            if kv.key == key {
+                return Some(&kv.value);
+            }
+
+            match &kv.next {
+                Some(_) => c = &kv.next,
+                None => c = &None,
+            }
+        }
+
+        None
+    }
+
+    pub fn insert(&mut self, key: K, value: V) {
+        let new_element = Rc::new(KeyValue::<K, V>::new(key, value, None));
+        if self.tail.is_none() {
+            self.head = Some(Rc::clone(&new_element));
+            self.tail = Some(Rc::clone(&new_element));
+        }
+
+        self.tail = Some(Rc::clone(&new_element));
+    }
+}
+
 #[derive(Clone, Debug)]
 struct KeyValue<K, V> {
-    key: K,
-    value: V,
-    next: Option<Box<KeyValue<K, V>>>,
+    pub key: K,
+    pub value: V,
+    pub next: Option<Rc<KeyValue<K, V>>>,
+}
+
+impl<K, V> KeyValue<K, V> {
+    fn new(key: K, value: V, next: Option<Rc<KeyValue<K, V>>>) -> Self {
+        Self { key, value, next }
+    }
 }
 
 impl<K, V> MyHashmap<K, V>
 where
-    K: PartialEq + Clone,
+    K: PartialEq + Clone + Hash,
     V: Clone,
 {
-    const INIT: Option<KeyValue<K, V>> = None;
     pub fn new() -> MyHashmap<K, V> {
         MyHashmap {
             size: DEFAULT_SIZE,
-            hash_elements: vec![Self::INIT; DEFAULT_SIZE as usize],
+            hash_elements: vec![Bucket::<K, V>::new(); DEFAULT_SIZE as usize],
             hasher: hash_key,
         }
     }
@@ -33,35 +87,38 @@ where
     pub fn with_capacity(size: u64) -> MyHashmap<K, V> {
         MyHashmap {
             size,
-            hash_elements: vec![Self::INIT; size as usize],
+            hash_elements: vec![Bucket::<K, V>::new(); size as usize],
             hasher: hash_key,
         }
     }
 
     pub fn insert(&mut self, key: K, value: V) {
-        let hash = (self.hasher)(key);
+        let hash = (self.hasher)(key.clone());
         let bucket_index = hash % self.hash_elements.len();
-
-        match self.hash_elements[bucket_index] {
-            // TODO: handle the data. We should create a new field in the bucket which holds a
-            // pointer to the last element in the bucket for quick insertions
-            Some(_) => println!("Some"),
-            None => println!("Nothing"),
-        }
-    }
-}
-
-impl<K, V> Default for MyHashmap<K, V>
-where
-    K: PartialEq + Clone,
-    V: Clone,
-{
-    fn default() -> Self {
-        Self::new()
+        self.hash_elements[bucket_index].insert(key.clone(), value);
     }
 }
 
 // TODO: Implement a real hash function
-fn hash_key<T>(key: T) -> usize {
-    return 1;
+fn hash_key<T: Hash>(key: T) -> usize {
+    let mut h = DefaultHasher::new();
+    key.hash(&mut h);
+    h.finish() as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hashmap_new() {
+        let hm = MyHashmap::<String, i32>::new();
+    }
+
+    #[test]
+    fn test_hashmap_insert() {
+        let mut hm = MyHashmap::<String, i32>::with_capacity(10);
+        hm.insert("hello".to_string(), 23);
+        println!("{:?}", hm)
+    }
 }
